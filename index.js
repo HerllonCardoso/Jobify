77b115b6492cfbeb1d3c6a4823a96962e2b5bc94
Herllon Cardoso <herllon.cardoso@alfalaval.com>
2020-07-13 09:00:49 -0300

    aula 2-1

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require ('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+    response.render('home')
+})
+
+app.get('/vaga', (request, response) => {
+    response.render('vaga')
+})
+
+
+app.listen(3000, (err) => {
+    if(err){
+        console.log("Nao foi possivel iniciar o servidor do Jobify")
+    } else {
+        console.log('Servidor rodando...')
+    }
+    })
